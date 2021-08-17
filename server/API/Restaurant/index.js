01e262229d296ce875701875f60646660458195c
AdithyakrishnaV <adithyakr007@gmail.com>
2021-08-17 21:49:22 +0530

    auth validation

--- server/API/Restaurant/index.js
+++ server/API/Restaurant/index.js
@@ -5,6 +5,10 @@ import passport from "passport";
 // Database model 
 import { RestaurantModel } from "../../database/allModels";
 
+//Validation
+import { ValidateRestaurantCity, ValidateRestaurantSearchString } from "../../validation/restaurant";
+import { ValidateRestaurantId } from "../../validation/food";
+
 const Router = express.Router();
 
 /*
@@ -16,6 +20,7 @@ Method   GET
 */
 Router.get("/", async (req, res) => {
     try {
+        await ValidateRestaurantCity(req.query);
         const { city } = req.query;
         const restaurants = await RestaurantModel.find({city});
 
@@ -34,6 +39,7 @@ Method   GET
 */
 Router.get("/:_id", async (req, res) => {
     try {
+        await ValidateRestaurantId(req.params);
         const { _id } = req.params;
         const restaurant = RestaurantModel.findOne(_id);
         if(!restaurant) 
@@ -54,6 +60,8 @@ Method   GET
 */
 Router.get("/search", async (req, res) => {
     try {
+        await ValidateRestaurantSearchString(req.body);
+
         const { searchString } = req.body;
         const restaurants = await RestaurantModel.find(
             { name: { $regex: searchString, $option: "i" } });
