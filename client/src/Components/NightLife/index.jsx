535c605037512ff53489c1974c39c4c0abfa9f73
AdithyakrishnaV <adithyakr007@gmail.com>
2021-08-22 23:01:19 +0530

    add night life

--- client/src/Components/NightLife/index.jsx
+++ client/src/Components/NightLife/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 //components
-import DiningCarousal from './DiningCarousal';
+import NightLifeCarousal from './NightLifeCarousal';
 
-const Dining = () => {
+const NightLife = () => {
     return (
         <div>
-            <DiningCarousal />
+            <NightLifeCarousal />
             <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
-                Dine-Out Restaurants in Vijay Nagar
+              Nightlife Restaurants in Vijay Nagar
             </h1>
         </div>
     );
 };
 
-export default Dining;
+export default NightLife;
