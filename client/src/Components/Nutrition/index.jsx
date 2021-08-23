0185f27c6e6ede55b9e73abd376e936c550108e9
AdithyakrishnaV <adithyakr007@gmail.com>
2021-08-23 23:35:41 +0530

    add nutrition card

--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -3,6 +3,8 @@ import React from "react";
 // components
 import NutritionHeroCarousal from "./NutritionHeroCarousal";
 import NutritionCarousal from "./NutritionCarousal";
+import NutritionCard from "./NutritionCard";
+
 
 const Nutrition = () => {
   return (
@@ -10,6 +12,36 @@ const Nutrition = () => {
       <NutritionHeroCarousal />
       <div className="my-6">
         <NutritionCarousal />
+      </div>
+      <div className="flex justify-between flex-wrap">
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
+        <NutritionCard 
+           bg="red"
+           image="https://dote.zmtcdn.com/prod/data/admin_assets/images/325/14a02cf7b28e5731928822d9e89a8325_1614756406.png"
+        />
       </div>
     </div>
   );
