82ee7a5315dea5e38e3164d74b5dbad3d5e777d7
AdithyakrishnaV <adithyakr007@gmail.com>
2021-08-28 17:18:17 +0530

    add restaurant and image reducer

--- server/API/Image/index.js
+++ server/API/Image/index.js
@@ -17,11 +17,29 @@ const upload = multer({ storage });
 
 
 /*
+Route     /
+Des       Get Image details
+Params    _id
+Access    Public
+Method    GET  
+*/
+Router.get("/:_id", async (req, res) => {
+    try {
+      const image = await ImageModel.findById(req.params._id);
+  
+      return res.json({ image });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+});
+
+
+/*
 Route     /image
 Des       Uploads given image to S3 bucket,  and saves file to mongodb
 Params    none
 Access    Public
-Method    GET  
+Method    post  
 */
 Router.post("/", upload.single("file"), async(req, res) => {
     try {
