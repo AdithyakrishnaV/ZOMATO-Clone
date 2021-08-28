19585d48a26b8a1e7759547cf7687d1ec3037352
AdithyakrishnaV <adithyakr007@gmail.com>
2021-08-28 13:01:42 +0530

    add redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Router, Redirect, Route } from "react-router-dom";
 // HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+import CheckoutLayoutHOC from "./HOC/Checkout.Hoc";
 
 // Component
 import Temp from "./Components/temp";
@@ -14,6 +15,7 @@ import OrderOnline from "./Page/Restaurant/OrderOnline";
 import Reviews from "./Page/Restaurant/Reviews";
 import Menu from "./Page/Restaurant/Menu";
 import Photos from "./Page/Photos";
+import Checkout from "./Page/Checkout";
 
 function App() {
   return (
@@ -30,6 +32,7 @@ function App() {
       <RestaurantLayoutHOC  path="/restaurant/:id/reviews" exact component={Reviews} />
       <RestaurantLayoutHOC  path="/restaurant/:id/menu" exact component={Menu} />
       <RestaurantLayoutHOC  path="/restaurant/:id/photos" exact component={Photos} />
+      <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
     </>
   );
 }
---
+++ client/src/Layout/Checkout.layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//component
+import Navbar from '../Components/Navbar/checkoutNavbar';
+
+const CheckoutLayout = (props) => {
+    return (
+        <>
+            <Navbar />
+            <div className="container mx-auto px-4 lg:px-20">
+                {props.children}
+            </div>
+        </>
+    );
+};
+
+export default CheckoutLayout;
